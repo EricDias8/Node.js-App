b40ed3da30d39ba90f9753d5733f00857b7f6579
EricDias8 <89018394+EricDias8@users.noreply.github.com>
2024-04-22 23:24:54 -0300

    database

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ require("express-async-errors");
 const express = require("express");
 const AppError = require("./utils/AppError")
 const routes = require("./routes");
+const database = require("./database/sqlite")
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
     if(error instanceof AppError){
